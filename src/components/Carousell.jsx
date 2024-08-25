a4671737942fb6f6a17bc79cd6a73b76453336d4
Damon Windsor <98252803+damonwindsor97@users.noreply.github.com>
2024-08-25 19:56:17 +1000

    webp images > png

--- src/components/Carousell.jsx
+++ src/components/Carousell.jsx
@@ -2,15 +2,15 @@ import HTML from '../assets/logos/html.png';
 import CSS from '../assets/logos/CSS-Logo.png';
 import Tailwind from '../assets/logos/tailwind.png';
 import VanillaX from '../assets/logos/vanilla-extract.svg'
-import JS from '../assets/logos/javascript.webp';
+import JS from '../assets/logos/javascript.png';
 import REACT from '../assets/logos/react.png';
 import Next from '../assets/logos/next.png'
 import Node from '../assets/logos/node.png';
 import Express from '../assets/logos/Expressjs.png';
 import Mongo from '../assets/logos/mongo.png';
-import Firebase from '../assets/logos/firebase.webp';
+import Firebase from '../assets/logos/firebase.png';
 import Github from '../assets/logos/github.png';
-import Figma from '../assets/logos/Figma.webp'
+import Figma from '../assets/logos/figma.png'
 
 import Marquee from "react-fast-marquee";
 
