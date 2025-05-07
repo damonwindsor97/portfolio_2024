53e10751dfed513eaad5f98bb9993f5d9313409e
Damon Windsor <damonwindsor@gmail.com>
2025-05-07 10:16:16 +1000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,7 @@ import About from "../components/About"
 function Home() {
   return (
     <div className="">
-      <div className="">
-        <Showcase/>
-      </div>
+
 
       <div className="p-4 md:p-0 md:max-w-[1000px] m-auto md:mt-20">
         <About/>
